bbde00dd31c7ca713822d0095b78db9fb3f4d215
Rudraksh Kapil <rudrakshkapil09@gmail.com>
2023-03-13 21:27:50 -0600

    Documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Integrated RGB-Thermal Orthomosaicking for Drone Imagery'
+copyright = '2023, Rudraksh Kapil'
+author = 'Rudraksh Kapil'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+version = '1.0.0'
 
 # -- General configuration
 
